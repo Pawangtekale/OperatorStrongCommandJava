64e314b44399787b1e285a282b88805a85f1579b
Pawan G Tekale <121091609+Pawangtekale@users.noreply.github.com>
2025-07-04 22:13:37 +0530

    Add files via upload

---
+++ LargestAmongThree.java
@@ -0,0 +1,9 @@
+class LargestAmongThree 
+{
+	public static void main(String[] args) 
+	{
+		int a=10,b=20,c=30;
+		//((a>b)?(a>c?"Largest is:"+a:"Largest is:"+c):(b>c)?"Largest is:"+b:"Largest is:"+c)
+		System.out.println(((a>b)?(a>c?"Largest is:"+a:"Largest is:"+c):(b>c)?"Largest is:"+b:"Largest is:"+c));
+	}
+}
