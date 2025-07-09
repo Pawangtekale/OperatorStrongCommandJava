52a1629cafd190d5fc5657dd0b7b3337c128b38d
Pawan G Tekale <121091609+Pawangtekale@users.noreply.github.com>
2025-07-09 21:53:57 +0530

    Add files via upload

---
+++ ExmapleIfElse.java
@@ -0,0 +1,13 @@
+class ExmapleIfElse 
+{
+	public static void main(String[] args) 
+	{
+		if (System.out.printf("Hello")!=null)
+		{
+			System.out.println("if Block");	
+		}else{
+			System.out.println("else Block");
+		}
+		
+	}
+}
